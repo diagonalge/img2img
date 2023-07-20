dccc0c917e4fc17ed8f3651bb654e9f048e70152
diagonalge <diagonalge@gmail.com>
2023-07-20 20:10:03 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import torch
+from PIL import Image
+from diffusers import StableDiffusionImg2ImgPipeline
+
+device = "cuda"
+model_id_or_path = "Linaqruf/anything-v3.0"
+pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path)
+pipe = pipe.to(device)
+pipe.load_textual_inversion("./baddream.pt", token = "baddream")
+pipe.load_textual_inversion("./easyneg.pt", token = "easyneg")
+pipe.load_textual_inversion("./fastneg.pt", token = "fastneg")
+
+image = Image.open("./image.jpg")
+
+
+prompt = "A (happy) guy with kid on beach, ((cute)), adorable, high quality"
+neg_prompt = "baddream, easyneg, fastneg, ((closed eyes))"
+
+
+image = pipe(prompt=prompt, negative_prompt = neg_prompt, image=image, strength=0.65, guidance_scale=6).images[0]
+image.save("result2.png")
